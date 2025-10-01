18ffa987f62508ff087c17bddec4fce60fe1ecbd
janubiyyy <tasyakhaerani66@gmail.com>
2025-10-01 16:26:47 +0700

    fixx all config

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
+const app_module_1 = require("./app.module");
+const config_1 = require("@nestjs/config");
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Global CORS
-  app.enableCors({
-    origin: (origin, callback) => {
-      callback(null, origin || true); // auto reflect semua origin
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
-  // Tambahin handler untuk preflight
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-    next();
-  });
-
-  await app.listen(3000, '0.0.0.0');
-  console.log(`🚀 Server running at http://0.0.0.0:3000`);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const configService = app.get(config_1.ConfigService);
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    app.enableCors();
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Task Management API')
+        .setDescription('A RESTful API for managing tasks')
+        .setVersion('1.0')
+        .addTag('tasks')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    const port = configService.get('PORT', 3000);
+    const host = configService.get('HOST', 'localhost');
+    await app.listen(port, host);
+    console.log(`Application is running on: http://${host}:${port}`);
+    console.log(`Swagger documentation is available at: http://${host}:${port}/api`);
 }
 bootstrap();
+//# sourceMappingURL=main.js.map
